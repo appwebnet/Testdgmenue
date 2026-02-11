9a452fe35d1077d065977e1060711c028749c7c7
app <alimahdaviwin1206@gmail.com>
2026-02-11 21:47:06 +0330

    Add files via upload
    Signed-off-by: app <alimahdaviwin1206@gmail.com>

---
+++ script.js
@@ -0,0 +1,78 @@
+
+const overlay = document.getElementById("overlay");
+let activeSheet = null;
+
+// باز کردن منو
+document.querySelectorAll("button[data-sheet]").forEach(btn => {
+    btn.addEventListener("click", () => {
+        const sheetId = btn.dataset.sheet;
+        openSheet(document.getElementById(sheetId));
+    });
+});
+
+function openSheet(sheet) {
+    if (activeSheet) closeSheet();
+    activeSheet = sheet;
+    sheet.classList.add("active");
+    overlay.classList.add("active");
+}
+
+function closeSheet() {
+    if (!activeSheet) return;
+    activeSheet.classList.remove("active");
+    overlay.classList.remove("active");
+    activeSheet = null;
+}
+
+overlay.onclick = closeSheet;
+
+// Drag logic
+document.querySelectorAll(".sheet").forEach(sheet => {
+    const handle = sheet.querySelector(".handle");
+
+    let startY = 0;
+    let currentY = 0;
+    let dragging = false;
+
+    handle.addEventListener("touchstart", e => {
+        startY = e.touches[0].clientY;
+        dragging = true;
+        sheet.style.transition = "none";
+    });
+
+    handle.addEventListener("touchmove", e => {
+        if (!dragging) return;
+        currentY = e.touches[0].clientY - startY;
+        if (currentY > 0) {
+            sheet.style.transform = `translateY(${currentY}px)`;
+        }
+    });
+
+    handle.addEventListener("touchend", () => {
+        dragging = false;
+        sheet.style.transition = "0.3s";
+
+        if (currentY > 120) {
+            closeSheet();
+            sheet.style.transform = "translateY(100%)";
+        } else {
+            sheet.style.transform = "translateY(0)";
+        }
+
+        currentY = 0;
+    });
+});
+
+
+
+
+
+function openModaledwarning() {
+    document.getElementById('modalOverlay').style.display = 'flex';
+    document.getElementById('main-content').classList.add('blurred');
+}
+
+function closeModaledwarning() {
+    document.getElementById('modalOverlay').style.display = 'none';
+    document.getElementById('main-content').classList.remove('blurred');
+}
